87484282b02f5207fccb1c578da9df7d2f06a4e8
kerkurt <kubilayerkurt@icloud.com>
2025-12-06 00:17:24 +0300

    Few touches to the report

--- main.py
+++ main.py
@@ -4,15 +4,17 @@ def main():
     book_path = "./books/frankenstein.txt"
     book_text = get_book_text(book_path)
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at: {book_path}...")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
     get_count_words(book_text)
-    print("------- Character Count --------")
+    print("--------- Character Count -------")
     char_count = get_count_each_character(book_text)
     char_count = sort_dictionary_by_value(char_count)
     for char, count in char_count.items():
         if char.isalpha():
             print(f"{char}: {count}")
+    
+    print("============= END ===============")
     
 def get_book_text(filePath):
     with open(filePath) as file:
