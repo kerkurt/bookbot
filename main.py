82db9c75d142dc30326c4466b5ce39f1a6bcfc3a
kerkurt <kubilayerkurt@icloud.com>
2025-12-05 23:59:29 +0300

    Added method for counting each character in book text.

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from stats import get_count_words
+from stats import get_count_words, get_count_each_character
 
 def main():
     book_path = "./books/frankenstein.txt"
     book_text = get_book_text(book_path)
     get_count_words(book_text)
-
+    char_count = get_count_each_character(book_text)
+    for char, count in char_count.items():
+        print(f"'{char}': {count}")
+    
 def get_book_text(filePath):
     with open(filePath) as file:
         return file.read()
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def get_count_words(text):
     words = text.split()
     print(f"Found {len(words)} total words")
+
+def get_count_each_character(text):
+    char_count = {}
+    for char in text:
+        lowered = char.lower()
+        if lowered in char_count:
+            char_count[lowered] += 1
+        else:
+            char_count[lowered] = 1
+    return char_count
